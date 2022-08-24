a0375d318ebdb6720722d3e7f6dc19de5699e4e9
Tyke Lee <tyke@Tykes-MacBook-Air.local>
2022-08-23 21:22:12 -0700

    titles on page as links

--- src/routes/news.js
+++ src/routes/news.js
@@ -7,8 +7,9 @@ newsRouter.get('', async(req, res) => {
 
   try {
     const newsAPI = await axios.get(`https://gnews.io/api/v4/search?q=example&token=29790cf991de1069b917b1cffe9dbde6&lang=en`)
-    // console.log(newsAPI.data)
-    res.render('news', { articles: newsAPI.data })
+    // console.log(newsAPI.data.articles)
+    res.render('news', { articles: newsAPI.data.articles })
+    // console.log(articles);
   } catch (error) {
     if (error.response){
       console.log(error.response.data)
