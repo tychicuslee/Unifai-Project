da8bab85d4e7263a2d83cb9e6c52fec17d3928ee
Tyke Lee <tyke@Tykes-MacBook-Air.local>
2022-08-24 18:17:24 -0700

    search

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-const express = require("express");
+const express = require('express');
+const bodyParser = require('body-parser')
 
 const app = express();
 const port = 3000;
+
+
 
 //Static Files
 app.use(express.static('public'))
@@ -12,6 +15,14 @@ app.use('/js', express.static(__dirname + 'public/js'))
 //Templating Engine
 app.set('views', './src/views/partials')
 app.set('view engine', 'ejs')
+
+app.use(bodyParser.urlencoded({ extended : true }))
+
+// const client = redis.createClient()
+// client.on('error', (err) => console.log('Redis Client Error', err));
+// await client.connect();
+// console.log('Redis connected!')
+
 
 //Routes
 const newsRouter = require('./src/routes/news')
--- src/routes/news.js
+++ src/routes/news.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const newsRouter = express.Router();
 const axios = require('axios')
+const redis = require('redis')
+
 
 newsRouter.get('', async(req, res) => {
   // res.render('news');
+  // const client = redis.createClient()
+  // client.on('error', (err) => console.log('Redis Client Error', err));
+  // await client.connect();
+  // console.log('Redis connected!')
+  
 
   try {
-    const newsAPI = await axios.get(`https://gnews.io/api/v4/search?q=example&token=29790cf991de1069b917b1cffe9dbde6&lang=en`)
+    // const cacheNews = await client.get('news')
+    // if (cacheNews){
+      // res.render('news', {articles: cacheNews})
+    // }
+
+    // else {
+      const newsAPI = await axios.get(`https://gnews.io/api/v4/search?q=example&token=29790cf991de1069b917b1cffe9dbde6&lang=en`)
     // console.log(newsAPI.data.articles)
     res.render('news', { articles: newsAPI.data.articles })
+    // }
     // console.log(articles);
   } catch (error) {
     if (error.response){
@@ -28,5 +42,65 @@ newsRouter.get('', async(req, res) => {
   }
 
 });
+
+newsRouter.post('', async(req, res) => {
+  let search = req.body.search
+  console.log(search)
+  try {
+
+    // const cacheNews = await client.get('news')
+    // if (cacheNews){
+    //   const uploadSearch = [];
+    //   // console.log(newsAPI.data.articles[0])
+    //   for (let i = 0; i < cacheNews.length; i++){
+
+    //     // console.log('this is articles title: ', newsAPI.data.articles[i].title)
+    //     // console.log('this is articles description: ', newsAPI.data.articles[i].description)
+    //     // console.log('this is articles content: ', newsAPI.data.articles[i].content)
+
+    //     if (cacheNews[i].title.toLowerCase().includes(search.toLowerCase()) || cacheNews[i].description.toLowerCase().includes(search.toLowerCase()) || cacheNews[i].content.toLowerCase().includes(search.toLowerCase())){
+    //       uploadSearch.push(cacheNews[i])
+    //     }
+    //   }
+    //   console.log('this is after the upload search: ', uploadSearch)
+    //   res.render('newsSearch', { articles: uploadSearch})
+    // }
+
+    // will have to change the end of this fetch request?
+    const newsAPI = await axios.get(`https://gnews.io/api/v4/search?q=example&token=29790cf991de1069b917b1cffe9dbde6&lang=en`)
+    // console.log('this is the data getting fetched: ', newsAPI.data.articles)
+    const uploadSearch = [];
+    // console.log(newsAPI.data.articles[0])
+    for (let i = 0; i < newsAPI.data.articles.length; i++){
+
+      // console.log('this is articles title: ', newsAPI.data.articles[i].title)
+      // console.log('this is articles description: ', newsAPI.data.articles[i].description)
+      // console.log('this is articles content: ', newsAPI.data.articles[i].content)
+      
+      if (newsAPI.data.articles[i].title.toLowerCase().includes(search.toLowerCase()) || newsAPI.data.articles[i].description.toLowerCase().includes(search.toLowerCase()) || newsAPI.data.articles[i].content.toLowerCase().includes(search.toLowerCase())){
+        uploadSearch.push(newsAPI.data.articles[i])
+      }
+    }
+    console.log('this is after the upload search: ', uploadSearch)
+    res.render('newsSearch', { articles: uploadSearch})
+    // console.log(articles);
+  } catch (error) {
+    if (error.response){
+      res.render('newsSearch', { articles: null})
+      console.log(error.response.data)
+      console.log(error.response.status)
+      console.log(error.response.headers)
+    }
+    else if (error.request){
+      res.render('newsSearch', { articles: null})
+      console.log(error.request)
+    }
+    else {
+      res.render('newsSearch', { articles: null})
+      console.error('Error', error.message)
+    }
+  }
+});
+
 
 module.exports = newsRouter
